ae267f777cee99f4b57c67d4a8cbd1b0b0842d13
SavvateevDV <119012158+SavvateevDV@users.noreply.github.com>
2023-01-29 21:57:03 +0300

    Add files via upload

---
+++ Лабораторная работа 2/library.py
@@ -0,0 +1,54 @@
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+class Book:
+    def __init__(self, id_: int, name: list, pages: int):  # прописываем атрибуты
+        self.id = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self):  # прописываем метод, возвращающий название книги
+        return f'Книга "{self.name}"'
+
+    def __repr__(self):  # прописываем метод, возвращающий валидную python строку
+        return f'Book(id_={self.id!r}, name={self.name!r}, pages={self.pages!r})'
+
+
+class Library:
+    def __init__(self, books=[]):  # прописываем атрибуты
+        self.books = books
+
+    def get_next_book_id(self):  # прописываем метод, возвращающий идентификатор для добавления новой книги в библиотеку
+        if not self.books:
+            return 1
+        return len(self.books) + 1
+
+    def get_index_by_book_id(self, id_Lib):  # прописываем метод, возвращающий индекс книги в списке
+        for i in range(len(self.books)):
+            if self.books[i].id == id_Lib:
+                return i
+        raise ValueError("Книги с запрашиваемым id не существует")
+
+
+if __name__ == '__main__':
+    empty_library = Library()  # инициализируем пустую библиотеку
+    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
